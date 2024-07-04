aae0e108cbf42da281b8dd7eacf2e5138ba2042a
calendar-test <gagetuckett@yahoo.com>
2024-07-04 15:44:16 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('inputBox').addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-        const inputText = event.target.value;
-        document.getElementById('outputBox').innerText = inputText;
-        event.target.value = '';
-    }
-});
